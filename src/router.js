4a839902b1b20ce4d0dec1eefb04983a8efa09cf
Mathias Jacobsen <mathiasrjacobsen@gmail.com>
2019-09-17 14:44:26 +0200

    added new files

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Codepen from './views/Codepen.vue'
+import Grid from './views/Grid.vue'
 
 Vue.use(Router)
 
@@ -10,14 +12,24 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+		},
+		{
+			path: '/codepen',
+			name: 'codepen',
+			component: Codepen
+		},
+		{
+			path: '/grid',
+			name: 'grid',
+			component: Grid
+		}
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
---
+++ src/views/Codepen.vue
@@ -0,0 +1,92 @@
+<template>
+  <nav>
+    <div class="container">
+      <ul class="navigation">
+        <li><router-link to="/">About Us</router-link></li>
+        <li><router-link to="/">Pricing</router-link></li>
+        <li><router-link to="/">Contact</router-link></li>
+      </ul>
+      <div class="buttons">
+        <a class="btn-main" href>Sign up</a>
+        <a class="btn-hot" href>Get a qoute</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "codepen"
+};
+</script>
+
+<style lang="sass" scoped>
+
+$color-primary: #f9ed69
+$color-secondary: #f08a5d
+$color-tertiary: #b83b5e
+$color-text-dark: #333333
+$color-text-light: #eeeeee
+
+$width-button: 150px
+
+@mixin align
+	display: flex
+	justify-content: space-between
+	align-items: center
+
+@mixin style-link-text($color)
+	text-decoration: none
+	text-transform: uppercase
+	color: $color
+
+//Sass Functions
+@function divide($a, $b)
+	@return $a/ $b
+
+nav
+	//margin: 30px
+	//using the function in action
+	margin: divide(60, 2) * 1px 
+	background-color: $color-primary 
+	
+	.container
+		@include align
+
+.navigation
+	list-style: none
+
+	li
+		display: inline-block
+		margin-left: 30px
+
+		&:first-child
+			margin-left: 0
+
+		a:link
+			@include style-link-text($color-text-dark)
+
+%btn-placeholder
+	padding: 10px
+	display: inline-block
+	text-align: center
+	border-radius: 100px
+	width: $width-button
+	@include style-link-text($color-text-light)
+
+.btn-main
+	&:link
+		@extend %btn-placeholder
+		background-color: $color-secondary
+	
+	&:hover
+		background-color: darken($color-secondary, 15%)
+
+.btn-hot
+	&:link
+		@extend %btn-placeholder
+		background-color: $color-tertiary
+
+	&:hover
+		background-color: lighten($color-tertiary, 10%)
+</style>
