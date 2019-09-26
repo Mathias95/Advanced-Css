f1c498e7cd4b9ab23e1b82df5d5e53e8cd4fae24
Mathias Jacobsen <mathiasrjacobsen@gmail.com>
2019-09-26 17:09:17 +0200

    setting up the nexter grid project

--- src/views/Nexter.vue
+++ src/views/Nexter.vue
@@ -1,6 +1,42 @@
   <template>
   <div class="nexter">
-		<h1>Nexter project</h1>
+		<div class="ncontainer">
+			<div class="nsidebar">
+				Sidebar
+			</div>
+
+			<header class="nheader">
+				Header
+			</header>
+
+			<div class="nrealtors">
+				Top 3 realtors
+			</div>
+
+			<section class="nfeatures">
+				Features
+			</section>
+
+			<div class="story__pictures">
+				Story pictures
+			</div>
+
+			<div class="story__content">
+				Story content
+			</div>
+
+			<section class="nhomes">
+				Homes
+			</section>
+
+			<section class="ngallery">
+				Gallery
+			</section>
+
+			<footer class="nfooter">
+				Footer
+			</footer>
+		</div>
   </div>
 </template>
 
@@ -11,3 +47,8 @@ export default {
 	components: {}
 };
 </script>
+
+<style lang="sass">
+@import "@/assets/sass/3-Nexter/main.sass"
+@import url('https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,400i|Nunito:300,300i&display=swap')
+</style>
