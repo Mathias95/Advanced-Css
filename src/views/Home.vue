801f372b9ea4dfbb6a925ae29095d49fcc66f91c
Mathias Jacobsen <mathiasrjacobsen@gmail.com>
2019-09-28 14:23:50 +0200

    finishing index page - needs grid for responsive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="home">
-		<h1>This is the welcome page, with thumbnails of the different project, with links</h1>
-		<router-link to="/natours">Natours project</router-link>
-		<router-link to="/trillo">Trillo project</router-link>
-		<router-link to="/nexter">Nexter project</router-link>
-		<router-link to="/grid">Grid Playground</router-link>
-  </div>
+  <section class="home">
+		<div class="home__container">
+			<h1 class="home__title">Advanced CSS Projects</h1>
+			<div class="home__links">
+				<div class="home__item">
+					<router-link class="home__link" to="/natours">
+						<img src="../assets/img/Natours-frontpage.png" alt="Project Natours">
+						<span>Natours project</span>
+					</router-link>
+				</div>
+				<div class="home__item">
+					<router-link class="home__link" to="/nexter">
+						<img src="../assets/img/Nexter-frontpage.png" alt="Project Nexter">
+						<span>Nexter project</span>
+					</router-link>
+				</div>
+				<div class="home__item">
+					<router-link class="home__link" to="/trillo">
+						<img src="../assets/img/Trillo-frontpage.png" alt="Project Trillo">
+						<span>Trillo project</span>
+					</router-link>
+				</div>
+			</div>
+		</div>
+  </section>
 </template>
 
 <script>
@@ -16,6 +34,53 @@ export default {
 };
 </script>
 
-<style lang="sass">
+<style lang="sass" scoped>
 
+.home
+	height: 100vh
+	display: flex
+	align-items: center
+	justify-content: center
+	font-family: sans-serif
+	background-color: #ffffff
+
+	&__container
+		max-width: 90%
+		margin: 0 auto
+		display: flex
+		flex-direction: column
+		align-items: center
+
+	&__title
+		margin-bottom: 8rem
+		color: #54483a
+		font-size: 3.5rem
+
+	&__links
+		display: grid
+		grid-template-columns: repeat(3, 1fr)
+		grid-gap: 10rem
+
+	&__item
+		text-align: center
+
+	&__link
+		text-decoration: none
+
+		img
+			display: block
+			width: 100%
+			transition: all 400ms
+			box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, 0.4)
+			border-radius: .5rem
+
+			&:hover
+				transform: translateY(-.8rem)
+				box-shadow: 0 2.5rem 4rem rgba(0, 0, 0, 0.5)
+
+		span
+			display: inline-block
+			margin-top: 2.5rem
+			color: #777777
+			font-size: 1.8rem
 </style>
